33e9223346ed9295f09047235a9ed6d7c6822287
Nichole O'Neill <nazm612@gmail.com>
2021-10-18 15:35:30 -0700

    installed tailwind

--- to-do/src/components/pages/Home.js
+++ to-do/src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <p>This is the home page</p>
         </div>
     );
--- to-do/src/components/pages/ToDo.js
+++ to-do/src/components/pages/ToDo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ToDo = () => {
     return (
-        <div>
+        <div className="w-64 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600">
             <p>These are the To-Do's</p>
         </div>
     );
