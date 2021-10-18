3288d778c81c274a3804f322b7f984aac40baa0a
Nichole O'Neill <nazm612@gmail.com>
2021-10-18 16:04:55 -0700

    uninstalled tailwind

--- to-do/src/components/pages/ToDo.js
+++ to-do/src/components/pages/ToDo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ToDo = () => {
     return (
-        <div className="w-64 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600">
+        <div className="todo">
             <p>These are the To-Do's</p>
         </div>
     );
